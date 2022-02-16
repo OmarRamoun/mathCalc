59b7ffaa5e830fb75fd7c0ce7d3692ebdbd3dc20
Fernando Herrera <fherrera0206@gmail.com>
2022-02-16 17:30:51 -0500

    Add test events with the interface

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,7 +14,7 @@ const Calculator = () => {
   return (
     <div className="col-md-9">
       <div className="row mx-0 row-cols-4 text-center">
-        <div className="bg-custom1 text-white text-end col-12 py-4 fs-5">
+        <div className="bg-custom1 text-white text-end col-12 py-4 fs-5" data-testid="result">
           {total}
           {operation}
           {next}
