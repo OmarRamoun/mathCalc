6114e7c017c8c8deb8c9cf137c6d2f88184e92de
Fernando Herrera <fherrera0206@gmail.com>
2022-02-16 18:01:32 -0500

    Add snapshot to header component

--- src/__tests__/header.test.js
+++ src/__tests__/header.test.js
@@ -1,7 +1,9 @@
+import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import Header from '../components/Header';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 test('Render components', () => {
-    const tree = renderer.create(<Header />);
+    const tree = renderer.create(<Router><Header /></Router>);
     expect(tree).toMatchSnapshot();
 });
